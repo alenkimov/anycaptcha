3307abf0ad11d4b86e3cae08e617bc6b847b4418
alenkimov <alen.kimov@gmail.com>
2024-10-04 04:29:08 +0500

    __init__ расширен

--- anycaptcha/__init__.py
+++ anycaptcha/__init__.py
@@ -21,6 +21,19 @@ from .errors import MalformedRequestError
 from .errors import BadInputDataError
 from .errors import UnableToSolveError
 
+# Captcha
+from .captcha.image import ImageCaptcha, ImageCaptchaSolution
+from .captcha.text import TextCaptcha, TextCaptchaSolution
+from .captcha.recaptcha_v2 import RecaptchaV2, RecaptchaV2Solution
+from .captcha.recaptcha_v3 import RecaptchaV3, RecaptchaV3Solution
+from .captcha.hcaptcha import HCaptcha, HCaptchaSolution
+from .captcha.funcaptcha import FunCaptcha, FunCaptchaSolution
+from .captcha.keycaptcha import KeyCaptcha, KeyCaptchaSolution
+from .captcha.geetest import GeeTest, GeeTestSolution
+from .captcha.geetest_v4 import GeeTestV4, GeeTestV4Solution
+from .captcha.capy import CapyPuzzle, CapyPuzzleSolution
+from .captcha.base import CaptchaType, BaseCaptchaSolution
+
 __all__ = [
     'Solver',
     'Service',
@@ -40,4 +53,27 @@ __all__ = [
     'MalformedRequestError',
     'BadInputDataError',
     'UnableToSolveError',
+    # Captcha
+    'ImageCaptcha',
+    'ImageCaptchaSolution',
+    'TextCaptcha',
+    'TextCaptchaSolution',
+    'RecaptchaV2',
+    'RecaptchaV2Solution',
+    'RecaptchaV3',
+    'RecaptchaV3Solution',
+    'HCaptcha',
+    'HCaptchaSolution',
+    'FunCaptcha',
+    'FunCaptchaSolution',
+    'KeyCaptcha',
+    'KeyCaptchaSolution',
+    'GeeTest',
+    'GeeTestSolution',
+    'GeeTestV4',
+    'GeeTestV4Solution',
+    'CapyPuzzle',
+    'CapyPuzzleSolution',
+    'CaptchaType',
+    'BaseCaptchaSolution',
 ]
